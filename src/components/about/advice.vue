1f5128e2223e1b40ae15de2bc12d51b5fe5f80e9
XJ <13211661891@163.com>
2019-03-25 16:59:41 +0800

    添加和修改关于我们和提供建议功能模块

---
+++ src/components/about/advice.vue
@@ -0,0 +1,44 @@
+<template>
+    <div align="center" style="margin-top: 35%">
+        <!--用户建议（跳转）-->
+    <!--<p>    输入框</p>-->
+        <div class="textarea-wrapper">
+            <div class="cube-textarea-wrapper" width="274px" height="100px">
+		<textarea  placeholder="建议和反馈" maxlength="60" indicator="[object Object]" class="cube-textarea">
+
+		</textarea>
+                <span class="cube-textarea-indicator" style="display: none;">
+		        60
+	            </span>
+            </div>
+        </div>
+        <div class="padding">
+            <button  onclick="alert('123')" class="button button-block button-positive">提交</button>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "advice",
+        data(){
+            return {
+                advice:{
+                    des:"",
+                    createUser:"vxiao",
+                    createTime:"2019-01-02 22:52:46",
+                    dataStatus:1,
+                }
+            };
+        },
+        methods:{
+
+            }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/about/version.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <div align="center" style="margin-top: 50%">
+        <div style="margin-top: 30%" class="bar bar-header">
+            <div class="h1 title">{{version.title}}</div>
+        </div>
+            <div class="card">
+                <div class="item item-text-wrap">
+                    {{version.des}}
+                </div>
+            </div>
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "version",
+        data(){
+            return{
+                version:{
+                    title:"关于我们",
+                    des:"二手贸易平台，版本号是1.0.0"
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
