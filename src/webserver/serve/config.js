9180accd46558a5fc61e6b14cd7e48ded2358748
aoxin <1364145571@qq.com>
2019-03-31 22:34:21 +0800

    冲突

--- src/webserver/serve/config.js
+++ src/webserver/serve/config.js
@@ -1,9 +1,5 @@
 const $config={
-<<<<<<< HEAD
-    baseUrl:'http://192.168.3.7:8081',
-=======
     baseUrl:'http://localhost:8080',
->>>>>>> c9867764fd540aba7cedb74acdc811e243cfb38e
     pageSize:'5',//每次请求列表加载的条数
 }
 export default $config
