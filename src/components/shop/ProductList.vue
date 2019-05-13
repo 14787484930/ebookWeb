2dcb2b278b99e08c2a94c4fa3d23a75ca36bccb5
gpj <1549361150@qq.com>
2019-05-13 19:36:06 +0800

    添加vuex购物车测试模块

---
+++ src/components/shop/ProductList.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul>
+        <li v-for="product in products" :key="product.id">
+            {{ product.title }} - {{ product.price }}
+            <br>
+            <button :disabled="!product.inventory" @click="addProductToCart(product)">
+                Add to cart
+            </button>
+        </li>
+    </ul>
+</template>
+
+<script>
+    import {mapState, mapActions} from 'vuex'
+
+    export default {
+        computed: mapState({
+            products: state => state.products.all
+        }),
+        methods: mapActions('cart', [
+            'addProductToCart'
+        ]),
+        created() {
+            this.$store.dispatch('products/getAllProducts')
+        }
+    }
+</script>
