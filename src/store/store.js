2eb627052f7d3c591b9f5b66da63df9287fe18b3
gpj <1549361150@qq.com>
2019-05-14 15:39:37 +0800

    修改store删除部分冗余代码

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import  format from '../webserver/serve/format'
+import format from '../webserver/serve/format'
 //购物车相关
 import cart from './modules/cat'
 import products from './modules/products'
@@ -8,30 +8,26 @@ import products from './modules/products'
 Vue.use(Vuex);
 const store = new Vuex.Store({
     state: {
-        flag:0,
-        user: {id:''}
+        flag: 0,
+        user: {
+            id: '',
+            flag: 0
+        }
     },
-    modules:{
+    modules: {
         cart,
         products
     },
-    getters:{
-        power(state){
-            if(state.user===undefined||state.user==null){
-                return false;
-            }
-            else{
-                return format.toStr(state.user.id).length>1
-            }
-
-        },
+    getters: {
         /*zxl 2019-04-15 新增权限的判断*/
-        power_flag(state){
-            return !(state.user === undefined || state.user == null || parseInt(state.user.flag) === 0);
-
+        power_flag(state) {
+            return !(parseInt(state.user.flag) === 0);
         },
-        getFlag(state){
-          return  state.flag;
+        power(state) {
+            return format.toStr(state.user.id).length > 1
+        },
+        getFlag(state) {
+            return state.flag;
         },
 
         getUser(state) {
@@ -40,15 +36,13 @@ const store = new Vuex.Store({
 
     },
     mutations: {
-        setFlag(state,val){
-            state.flag=val;
+        setFlag(state, val) {
+            state.flag = val;
         },
-        setUser (state,info) {
-            state.user=info;
+        setUser(state, info) {
+            state.user = info;
         }
     },
-    actions: {
-
-    }
+    actions: {}
 })
-export  default  store
+export default store
